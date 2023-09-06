eacff34c848e5d3679da6ca685695d2b435ac094
zunda <zunda.dev@gmail.com>
2023-09-06 18:06:39 +0900

    fix CodeBlockView

--- Sources/MarkdownUI/Components/CodeBlockView.swift
+++ Sources/MarkdownUI/Components/CodeBlockView.swift
@@ -8,10 +8,13 @@ import Markdown
 struct CodeBlockView: View {
   let language: String?
   let sourceCode: String
+  var fileName: String.SubSequence? {
+    language?.split(separator: ":", maxSplits: 1).last
+  }
   
   var body: some View {
     VStack(alignment: .leading, spacing: 0) {
-      if let fileName = language?.split(separator: ":")[safe: 1] {
+      if let fileName {
         Text(fileName.trimming(while: \.isWhitespace))
           .padding(.horizontal, 5)
           .padding(.vertical, 2)
@@ -33,7 +36,7 @@ struct CodeBlockView: View {
         .foregroundStyle(.background)
         .background {
           CustomRoundedRectangle(
-            topLeftRadius: 0,
+            topLeftRadius: fileName == nil ? 8 : 0,
             topRightRadius: 8,
             bottomLeftRadius: 8,
             bottomRightRadius: 8
@@ -41,12 +44,6 @@ struct CodeBlockView: View {
             .foregroundStyle(.foreground)
         }
     }
-  }
-}
-
-private extension Array {
-  subscript(safe index: Index) -> Element? {
-    return indices.contains(index) ? self[index] : nil
   }
 }
 
