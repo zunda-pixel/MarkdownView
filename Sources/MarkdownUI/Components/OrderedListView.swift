999f5aa056663d416cdf7fbba7d57c3f9fc690e6
zunda <zunda.dev@gmail.com>
2023-09-07 01:52:44 +0900

    fix OrderedListView

--- Sources/MarkdownUI/Components/OrderedListView.swift
+++ Sources/MarkdownUI/Components/OrderedListView.swift
@@ -13,10 +13,41 @@ struct OrderedListView: View {
   var body: some View {
     VStack(alignment: .leading, spacing: 5) {
       ForEach(items.indexed(), id: \.index) { index, item in
-        HStack(alignment: .center, spacing: 5) {
-          SwiftUI.Text("\(index + 1).")
-          ForEach(item.children.indexed(), id: \.index) { _, child in
-            MarkupContentView(content: child, listDepth: listDepth, isNested: true)
+        var unorderedListContains: Bool {
+          item.children.contains { child in
+            if case .unorderedList(_) = child {
+              return true
+            } else {
+              return false
+            }
+          }
+        }
+        
+        if unorderedListContains {
+          VStack(alignment: .leading, spacing: 5) {
+            if let child = item.children.first {
+              FlowLayout {
+                SwiftUI.Text("\(index + 1).")
+                MarkupContentView(content: child, listDepth: listDepth, isNested: true)
+              }
+            }
+            
+            if item.children.count > 1 {
+              ForEach(1..<item.children.count, id: \.self) { i in
+                let child = item.children[i]
+                HStack(alignment: .center, spacing: 5) {
+                  Spacer().frame(width: 10)
+                  MarkupContentView(content: child, listDepth: listDepth + 1, isNested: true)
+                }
+              }
+            }
+          }
+        } else {
+          HStack(alignment: .center, spacing: 5) {
+            SwiftUI.Text("\(index + 1).")
+            ForEach(item.children.indexed(), id: \.index) { _, child in
+              MarkupContentView(content: child, listDepth: listDepth, isNested: true)
+            }
           }
         }
       }
@@ -53,11 +84,14 @@ struct OrderedListView: View {
 
 #Preview {
   let document = Document(parsing: """
-1. Hello1
-1. Hello2
-1. Hello3
-1. Hello4
-1. Hello5
+1. First list item
+   - First nested list item
+     - Second nested list item
+     - Second nested list item
+
+1. Item1
+1. Item2
+1. Item4
 """)
 
   return List {
--- Sources/MarkdownUI/MarkdownEditor.swift
+++ Sources/MarkdownUI/MarkdownEditor.swift
@@ -64,5 +64,9 @@ hello
 
 - First nested list item
 - Second nested list item
+
+1. First list item
+   - First nested list item
+     - Second nested list item
 """)
 }
