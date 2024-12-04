16f97e2bc4eac549017f4df8572638337cd83320
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-12-04 21:03:28 +0900

    update to swift 6.0 (#21)

--- Tests/MarkdownViewTests/MarkdownViewParserTests.swift
+++ Tests/MarkdownViewTests/MarkdownViewParserTests.swift
@@ -1,27 +1,18 @@
 import Algorithms
 import Markdown
-import XCTest
+import Testing
 
 @testable import MarkdownView
 
-final class MarkdownViewParserTests: XCTestCase {
-  func testExample() throws {
-    // XCTest Documentation
-    // https://developer.apple.com/documentation/xctest
+@Test
+func testFileExtensionNameParse() {
+  let content = "swift: Sample.swift "
+  let elements = content.split(separator: ":", maxSplits: 1)
+  let fileExtension = elements[0]
+  let fileName = elements[1]
 
-    // Defining Test Cases and Test Methods
-    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-  }
-
-  func testFileExtensionNameParse() {
-    let content = "swift: Sample.swift "
-    let elements = content.split(separator: ":", maxSplits: 1)
-    let fileExtension = elements[0]
-    let fileName = elements[1]
-
-    XCTAssertEqual(fileExtension, "swift")
-    XCTAssertEqual(fileName.trimming(while: \.isWhitespace), "Sample.swift")
-  }
+  #expect(fileExtension == "swift")
+  #expect(fileName.trimming(while: \.isWhitespace) == "Sample.swift")
 }
 
 func printMarkup(markup: Markup, depth: Int) {
