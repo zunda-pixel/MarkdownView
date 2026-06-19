cbf2dfeb7267d33ae4f7d2b656bd88578572c974
Yazawa Hiroki <47569369+zunda-pixel@users.noreply.github.com>
2026-06-19 10:36:45 +0900

    add retroactive

--- Sources/MarkdownView/Contents/MarkupContent.swift
+++ Sources/MarkdownView/Contents/MarkupContent.swift
@@ -30,4 +30,4 @@ public enum MarkupContent: Hashable, Sendable {
   case unhandled(content: String)
 }
 
-extension Aside.Kind: @unchecked Sendable, Hashable {}
+extension Aside.Kind: @unchecked @retroactive Sendable, @retroactive Hashable {}
